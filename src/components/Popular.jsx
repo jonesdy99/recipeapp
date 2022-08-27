43bc2f51587b2ff069523d3011f0ec4e1b664122
Jonesdy99 <jonesdylan1165@gmail.com>
2022-08-27 15:45:52 -0400

    implement splide for carousel

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react"
+import styled from "styled-components"
+import {Splide, SplideSlide} from '@splidejs/react-splide'
+import '@splidejs/splide/dist/css/splide.min.css'
 
 
 function Popular() {
@@ -16,14 +19,39 @@ function Popular() {
   }
 
   return (
-    <div>{popular.map((recipe) => {
+    <div>
       return(
-        <div>
-          <p>{recipe.title}</p>
-        </div>
+        <Wrapper>
+          <h3>Popular Dishes</h3>
+          <Splide>
+          {popular.map((recipe) => {
+            return(
+              <SplideSlide>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title}/>
+                </Card>
+              </SplideSlide>
+            )
+          })}
+          </Splide>
+        </Wrapper>
       )
-    })}</div>
+    </div>
   )
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem
+`
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img{
+    border-radius: 2rem;
+  }
+`
 
 export default Popular
