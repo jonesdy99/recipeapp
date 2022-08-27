aca1a70cd7bf423f1af829f4503828d1de5b0321
Jonesdy99 <jonesdylan1165@gmail.com>
2022-08-27 15:19:58 -0400

    implement api

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,12 @@
+
+
 function Popular() {
+
+  const getPopular = async () => {
+    const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+    const data = await api.json()
+  }
+
   return (
     <div>Popular</div>
   )
