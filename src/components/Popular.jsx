12a92c695f8120552d1320bc233c2e1823ffea52
Jonesdy99 <jonesdylan1165@gmail.com>
2022-08-28 16:48:18 -0400

    implement a check feature that checks local storage for data and renders that to keep API requests at a minium

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -13,9 +13,16 @@ function Popular() {
   },[])
 
   const getPopular = async () => {
-    const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
-    const data = await api.json()
-    setPopular(data.recipes)
+
+    const check = localStorage.getItem('popular')
+    if(check){
+      setPopular(JSON.parse(check))
+    } else {
+      const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+      const data = await api.json()
+      localStorage.setItem('popular', JSON.stringify(data.recipes))
+      setPopular(data.recipes)
+    }
   }
 
   return (
@@ -31,7 +38,7 @@ function Popular() {
           }}>
           {popular.map((recipe) => {
             return(
-              <SplideSlide>
+              <SplideSlide key={recipe.id}>
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title}/>
