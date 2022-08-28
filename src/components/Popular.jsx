3abac9d7b152815e49b8c313fcece93ceec37da1
Jonesdy99 <jonesdylan1165@gmail.com>
2022-08-28 16:42:46 -0400

    style home page

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -20,23 +20,28 @@ function Popular() {
 
   return (
     <div>
-      return(
         <Wrapper>
           <h3>Popular Dishes</h3>
-          <Splide>
+          <Splide options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: 'free',
+            gap: '5rem',
+          }}>
           {popular.map((recipe) => {
             return(
               <SplideSlide>
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title}/>
+                  <Gradient/>
                 </Card>
               </SplideSlide>
             )
           })}
           </Splide>
         </Wrapper>
-      )
     </div>
   )
 }
@@ -51,7 +56,36 @@ const Card = styled.div`
 
   img{
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+  p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
 `
 
 export default Popular
